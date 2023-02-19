c3b0630e9cda258c50d1a974250d095b0c57bff7
Mary8992 <mc046569@gmail.com>
2023-02-19 17:49:16 +0300

    product image change and curve color change functionality added

---
+++ app.js
@@ -0,0 +1,8 @@
+function imgSlider(product) {
+    document.querySelector('.starbucks').src = product;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
